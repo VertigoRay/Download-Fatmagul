d873dba96e4ca2e4daf0648c693e4510cdecfb24
Raymond Piller <ray@unt.edu>
2016-04-21 13:33:54 -0500

    Had functions in wrong files. :D

--- functions/ConvertFrom-PlexNamingConvention.ps1
+++ functions/ConvertFrom-PlexNamingConvention.ps1
@@ -11,31 +11,14 @@ function ConvertFrom-PlexNamingConvention () {
     )
 
     BEGIN {
-        Write-Verbose "Converting From: Plex Naming Convention"
+        Write-Verbose "Converting To: Plex Naming Convention"
     }
 
     PROCESS {
+        # since I know what the filenames where, I'm not going to do anything magical.
+
         foreach ($File in $FileName) {
-            $child = Split-Path $File -Child
-            $ext = $child.Split('.')[-1]
-            $title = $child.Split('.')[0..($child.Split('.').Count-2)] -Join ' '
-
-            [System.Collections.ArrayList]$title_pieces = @()
-            foreach ($t in $title.Split(' .')) {
-                try {
-                    [int]$episode = $t
-                } catch [System.Management.Automation.RuntimeException] {
-                    $title_piece = (Get-Culture).TextInfo.ToTitleCase($t)
-                }
-
-                if ($episode) {
-                    $title_pieces.Add(('e{0:d3}' -f $episode))
-                } else {
-                    $title_pieces.Add($title_piece)
-                }
-            }
-
-            $rename_to = '{0}.{1}' -f @(($title_pieces -join '.'), $ext)
+            $rename_to = $File.Name.Replace('.e', ' ')
             Write-Verbose "$($File.Name) > ${rename_to}"
             Rename-Item $File.FullName $rename_to -ErrorAction Ignore
         }
